606060663a024da65dcb624a3e0193873c063fe3
Nevrit Ahua <nevritahua.na@gmail.com>
2024-03-21 11:41:19 +0000

    Working on the database. Personal informations is made

--- person_informations/admin.py
+++ person_informations/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Information
 
 # Register your models here.
+class PersonnalInfoAdmin(admin.ModelAdmin):
+    list_display = ["name", "last_name", "email", "phone_number", "position"]
+    
+admin.site.register(Information, PersonnalInfoAdmin)
