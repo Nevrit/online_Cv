b10f1c72ce5632764c5c00a4bd8d54fce8ce90d2
Nevrit Ahua <nevritahua.na@gmail.com>
2024-03-22 12:15:15 +0000

    Add carousel galery

--- person_informations/admin.py
+++ person_informations/admin.py
@@ -1,17 +1,28 @@
 from django.contrib import admin
-from .models import Information, ExpertiseLanguage, ExpertiseFramework
+from .models import Information, ExpertiseLanguage, ExpertiseFramework, Portfolio, CarouselPortfolioImage
 
 class PersonnalInfoAdmin(admin.ModelAdmin):
-    list_display = ["name", "last_name", "email", "phone_number", "position"]
+    list_display = ["name", "last_name", "email", "phone_number", "position", "create_at", "update_at"]
     
 class ExpertiseLanguageAdmin(admin.ModelAdmin):
-    list_display = ["name", "percentage_value"]
+    list_display = ["name", "percentage_value", "create_at", "update_at"]
     
 
 class ExpertiseFrameworkAdmin(admin.ModelAdmin):
-    list_display = ["name", "percentage_value"]
+    list_display = ["name", "percentage_value", "create_at", "update_at"]
+
+class ImageActualiteInline(admin.TabularInline):
+    model = CarouselPortfolioImage
+    readonly_fields = ('id',)
+    extra = 2
+    show_change_link = True
+
+class PortfolioAdmin(admin.ModelAdmin):
+    list_display = ["card_title", "create_at", "update_at"]
+    inlines = [ImageActualiteInline]
     
     
 admin.site.register(Information, PersonnalInfoAdmin)
 admin.site.register(ExpertiseLanguage, ExpertiseLanguageAdmin)
 admin.site.register(ExpertiseFramework, ExpertiseFrameworkAdmin)
+admin.site.register(Portfolio, PortfolioAdmin)
--- person_informations/models.py
+++ person_informations/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+# Personal informations
 class Information(models.Model):
     
     BAC = "BAC"
@@ -34,8 +34,9 @@ class Information(models.Model):
     position = models.CharField(max_length=100, verbose_name="Titre du poste occupé")
     company = models.CharField(max_length=100, verbose_name="Nom de la société")
     hiring_date = models.DateField(verbose_name="Date d'embauche")
-    
-    
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
     class Meta:
         verbose_name = 'Information Personnelles'
         
@@ -48,7 +49,8 @@ class ExpertiseLanguage(models.Model):
     name = models.CharField(max_length=30, verbose_name="Nom du langage")
     icon = models.CharField(max_length=50, verbose_name="Classe de l'icône FontAwesome")
     percentage_value = models.IntegerField(default=0, choices=((i,i) for i in range(0, 101)), verbose_name="Niveau de connaisance")
-
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
     
     class Meta:
         verbose_name = 'Langage Informatique'
@@ -63,4 +65,41 @@ class ExpertiseFramework(models.Model):
     name = models.CharField(max_length=30, verbose_name="Nom du langage")
     icon = models.CharField(max_length=50, verbose_name="Classe de l'icône FontAwesome")
     percentage_value = models.IntegerField(default=0, choices=((i,i) for i in range(0, 101)), verbose_name="Niveau de connaisance")
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Framework'
     
+
+# Portfolio
+class Portfolio(models.Model):
+    card_image = models.ImageField(upload_to='portfolio/', height_field=None, width_field=None, max_length=100, verbose_name="Image de la card")
+    card_title = models.CharField(max_length=50, verbose_name="Titre")
+    description = models.TextField(max_length=300, verbose_name="Description")
+    href = models.CharField(max_length=50, verbose_name="Le lien", help_text="Le lien que vous mettrez ici devra être le même que le id")
+    id = models.CharField(max_length=50, verbose_name="id", help_text="Le id que vous mettrez ici devra être unique", unique=True, primary_key=True)
+    aria_labelledby = models.CharField(max_length=100, verbose_name="aria-labelledby", editable=False, blank=True)
+    carousel_image = models.ImageField(upload_to="carousel_portfolio_images/", height_field=None, width_field=None, max_length=100, verbose_name="images de la carousselle")
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        verbose_name = 'Portfolio'
+    
+    # This fonction allow to add "Label" at the end of the id
+    def save(self, *args, **kwargs):
+        self.aria_labelledby = f"{self.id}Label"
+        super(Portfolio, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.card_title
+    
+class CarouselPortfolioImage(models.Model):
+    portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE, null=True, blank=True)
+    image = models.ImageField(null=False,upload_to='galery/', verbose_name='Image')
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Carousel image'
