ff1ec8a4a544408fcb028d82de923a0c3c56c499
Nevrit Ahua <nevritahua.na@gmail.com>
2024-03-22 13:07:00 +0000

    Add portfolio images via carousel through card bouton

--- person_informations/views.py
+++ person_informations/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from .models import Information, ExpertiseLanguage, ExpertiseFramework
+from .models import Information, ExpertiseLanguage, ExpertiseFramework, Portfolio
 
 # Create your views here.
 def index(request):
     person_informations = Information.objects.all()
     languages = ExpertiseLanguage.objects.all()
     framework = ExpertiseFramework.objects.all()
-    context = {"person_informations" : person_informations, "languages" : languages, "framework":framework}
+    portfolios = Portfolio.objects.all()
+    context = {"person_informations" : person_informations, "languages" : languages, "framework":framework, "portfolios":portfolios}
     return render(request, 'index/index.html', context)
