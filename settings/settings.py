f54459ef546d802869b324fb58467669db5d72ab
Nevrit Ahua <nevritahua.na@gmail.com>
2024-03-28 10:44:31 +0000

    Installing xhtml2pdf. Insert CV to download link

--- settings/settings.py
+++ settings/settings.py
@@ -160,5 +160,5 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'nevritahua.na@gmail.com'
-EMAIL_HOST_PASSWORD = 'jeaxvuenborvcozj'
+EMAIL_HOST_PASSWORD = ''
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
